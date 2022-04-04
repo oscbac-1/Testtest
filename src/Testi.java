6aae296c82728738148409360ded44464274af3d
Oscar Backhouse <oscbac-1@ltu.student.se>
2022-04-04 18:34:59 +1000

    Initial commit

---
+++ src/Testi.java
@@ -0,0 +1,11 @@
+public class Testi {
+    public static void main(String[] args) {
+
+        String Username;
+        String Password;
+
+        Username = "Hej";
+        Password = "TestavIT";
+    }
+
+}
---
+++ src/TestiTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestiTest {
+
+    @org.junit.jupiter.api.Test
+    public void main() {
+        String Password ;
+        Password = "TestavIT";
+        assertEquals("Testav123", Password);
+
+    }
+}
